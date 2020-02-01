d8449e910fb8a2ccccb69f7e1d270421b4ca93ba
David Herve <david@localhost.localdomain>
2020-02-01 14:06:57 +0100

    sprite fait

--- sprite.py
+++ sprite.py
@@ -15,9 +15,35 @@ class Sprite(pygame.sprite.Sprite):
 
         # Images du sprite pour l'animation
         self.images = chargeImages(nomFichier)
+
+        # Indice des images pour l'animation
         self.index = 0
 
-
+        # Image actual du sprite
+        self.image = self.images[self.index]
 
         # Hitbox rectangulaire
-        self.rect = self.images[0].image.get_rect()
+        self.rect = self.image.get_rect()
+
+        # Nombre de frames
+        self.frames = len(self.images)
+
+        # Temps d'une frame
+        self.temps = 1 / self.frames
+
+        # Temps actuel de la frame
+        self.frameActuel = 0
+
+    # Anime le sprite par rapport au temps
+    def animeSpriteTemps(self, temps: float):
+
+        self.frameActuel += temps
+
+        if self.frameActuel >= self.temps:
+            self.frameActuel = 0
+            self.index = int((self.index + 1) % self.frames)
+            self.image = self.images[self.index]
+
+    # Anime le sprite pour le groupe d'un sprite
+    def update(self, dt):
+        self.animeSpriteTemps(dt)
