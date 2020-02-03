d579c1245624ef20f8dd80199811e2fd4ab305bc
HERVE David <herved@hostname.iut2.upmf-grenoble.fr>
2020-02-03 17:15:08 +0100

    style

--- player.py
+++ player.py
@@ -3,15 +3,18 @@ from sprite import Sprite
 
 class Player():
 
+    a = 2
+
     def __init__(self, x, y):
 
         self.sprite = Sprite('Assets/RobocopWalk.png', 8, 1, 5)
         self.x, self.y = x, y
+        self.velocity = 10
 
     def draw(self, surface):
-        if self.move() == 1:
+        if self.move() == pygame.K_RIGHT:
             self.sprite.draw(surface, self.x, self.y)
-        elif self.move() == 2:
+        elif self.move() == pygame.K_LEFT:
             self.sprite.draw(surface, self.x, self.y, True)
         else:
             self.sprite.draw(surface, self.x, self.y)
@@ -21,11 +24,11 @@ class Player():
         keys = pygame.key.get_pressed()
 
         if keys[pygame.K_RIGHT]:
-            self.x += 10
-            return 1
+            self.x += self.velocity
+            return pygame.K_RIGHT
 
         if keys[pygame.K_LEFT]:
-            self.x -= 10
-            return 2
+            self.x -= self.velocity
+            return pygame.K_LEFT
 
-        return 0
+        return None
