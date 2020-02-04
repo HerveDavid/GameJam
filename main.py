5a24069b12648997f5a6325fd0d5fdec71a34d73
herved <HerveDavid>
2020-02-04 13:58:05 +0100

    ok

--- main.py
+++ main.py
@@ -1,22 +1,22 @@
 import pygame
 from utilitaries import *
 from colors import COLORS
-from sprite import Sprite
+# from sprite import Sprite
 from player import Player
-from ennemy import Ennemy
-from bouton import Bouton
+# from ennemy import Ennemy
+# from bouton import Bouton
 
 
 def main(screen: pygame.display) -> None:
 
     player = Player(300, 300)
-    ennemy = Ennemy(300,300)
-    button = Button(300,300)
+    # ennemy = Ennemy(300,300)
+    # button = Button(300,300)
     while RUNNING:
         events()
-        ennemy.draw(screen)
+        # ennemy.draw(screen)
         player.draw(screen)
-        button.draw(screen)
+        # button.draw(screen)
 
         pygame.display.update()
         screen.fill(COLORS['white'])
