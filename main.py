d2da79d5f5e63b32afa68b45dc4ac121cb383c7f
herved <HerveDavid>
2020-02-04 13:51:35 +0100

    jump fini

--- main.py
+++ main.py
@@ -1,24 +1,30 @@
 import pygame
 from utilitaries import *
 from colors import COLORS
+# from sprite import Sprite
 from player import Player
+# from ennemy import Ennemy
+# from bouton import Bouton
 
-# Procedure pour le lancer le jeu
-def game(screen: pygame.display) -> None:
 
-    player = Player(300, 100)
+def main(screen: pygame.display) -> None:
 
+    player = Player(300, 300)
+    # ennemy = Ennemy(300,300)
+    # button = Button(300,300)
     while RUNNING:
         events()
-
+        # ennemy.draw(screen)
         player.draw(screen)
+        # button.draw(screen)
 
         pygame.display.update()
         screen.fill(COLORS['white'])
-
+        
         pygame.time.Clock().tick(FPS)
 
 
 if __name__ == '__main__':
+
     screen = init()
-    game(screen)
+    main(screen)
