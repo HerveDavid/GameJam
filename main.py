998676fde61495cecb1b07f9154cd95c9d4e67d9
herved <HerveDavid>
2020-02-05 14:19:33 +0100

    all

--- main.py
+++ main.py
@@ -20,40 +20,40 @@ def main(screen: pygame.display) -> None:
     game = Game(etage1, player, enemies)
     clock = pygame.time.Clock()
 
-    # game.initMixer()
-    #
-    # sound_channel_player = pygame.mixer.Channel(0)
-    # sound_channel_ennemy = pygame.mixer.Channel(1)
-    # sound_channel_ambient = pygame.mixer.Channel(2)
-    #
-    # channels = []
-    # channels.append(sound_channel_player)
-    # channels.append(sound_channel_ennemy)
-    # channels.append(sound_channel_ambient)
-    #
-    # sound_player_jump = pygame.mixer.Sound("Audio/Fx/jump.ogg")
-    #
-    # sounds_player = []
-    # sounds_player.append(sound_player_jump)
-    #
-    # sound_minotaur1 = pygame.mixer.Sound("Audio/Fx/minotaur1.wav")
-    # sound_minotaur2 = pygame.mixer.Sound("Audio/Fx/minotaur2.wav")
-    #
-    # sounds_ennemy = []
-    # sounds_ennemy.append(sound_minotaur1)
-    # sounds_ennemy.append(sound_minotaur2)
-    #
-    # sound_blow = pygame.mixer.Sound("Audio/Fx/blow.wav")
-    #
-    # sounds_ambient = []
-    # sounds_ambient.append(sound_blow)
+    game.initMixer()
+
+    sound_channel_player = pygame.mixer.Channel(0)
+    sound_channel_ennemy = pygame.mixer.Channel(1)
+    sound_channel_ambient = pygame.mixer.Channel(2)
+
+    channels = []
+    channels.append(sound_channel_player)
+    channels.append(sound_channel_ennemy)
+    channels.append(sound_channel_ambient)
+
+    sound_player_jump = pygame.mixer.Sound("Audio/Fx/jump.ogg")
+
+    sounds_player = []
+    sounds_player.append(sound_player_jump)
+
+    sound_minotaur1 = pygame.mixer.Sound("Audio/Fx/minotaur1.wav")
+    sound_minotaur2 = pygame.mixer.Sound("Audio/Fx/minotaur2.wav")
+
+    sounds_ennemy = []
+    sounds_ennemy.append(sound_minotaur1)
+    sounds_ennemy.append(sound_minotaur2)
+
+    sound_blow = pygame.mixer.Sound("Audio/Fx/blow.wav")
+
+    sounds_ambient = []
+    sounds_ambient.append(sound_blow)
 
     while RUNNING:
         events()
 
         game.display(screen)
-        # game.playMixerEnnemy(channels[1], sounds_ennemy)
-        # game.playMixerPlayer(channels[0], sounds_player)
+        game.playMixerEnnemy(channels[1], sounds_ennemy)
+        game.playMixerPlayer(channels[0], sounds_player)
 
         pygame.display.update()
         screen.fill(COLORS['black'])
